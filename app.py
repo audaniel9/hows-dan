ba974856ca4401219eab01229f4e3f483661c657
Daniel Au <audaniel9@gmail.com>
2026-04-26 17:34:40 -0400

    converted to single page app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 
@@ -7,16 +7,7 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/danspotting")
-def danspotting():
-    return render_template("danspotting.html")
-
-@app.route("/food")
-def food():
+def get_photos():
     food_dir = os.path.join(app.static_folder, "food")
     photos = []
     if os.path.exists(food_dir):
@@ -29,7 +20,31 @@ def food():
             for f in os.listdir(food_dir)
             if f.lower().endswith((".jpg", ".jpeg", ".png", ".webp"))
         ]
-    return render_template("food.html", photos=photos)
+    return photos
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/danspotting")
+def danspotting():
+    return render_template("danspotting.html")
+
+@app.route("/food")
+def food():
+    return render_template("food.html", photos=get_photos())
+
+@app.route("/partial/")
+def partial_home():
+    return render_template("partials/home_fragment.html")
+
+@app.route("/partial/danspotting")
+def partial_danspotting():
+    return render_template("partials/danspotting_fragment.html")
+
+@app.route("/partial/food")
+def partial_food():
+    return render_template("partials/food_fragment.html", photos=get_photos())
 
 if __name__ == "__main__":
     app.run(debug=True)
